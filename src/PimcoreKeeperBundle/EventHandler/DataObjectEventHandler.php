4e6b9302e46eb5f541b5feebe4250841fb030d41
Wojciech Peisert <wojtekp@sauron.pl>
2021-08-23 15:02:58 +0200

    databject handler in progress...

--- src/PimcoreKeeperBundle/EventHandler/DataObjectEventHandler.php
+++ src/PimcoreKeeperBundle/EventHandler/DataObjectEventHandler.php
@@ -3,13 +3,14 @@
 namespace Pipirima\PimcoreKeeperBundle\EventHandler;
 
 use Pimcore\Event\Model\DataObject\ClassDefinitionEvent;
+use Pimcore\Event\Model\DataObjectEvent;
 use Pimcore\Tool;
 use Pipirima\PimcoreKeeperBundle\Service\EventHandlerInterface;
 use Pipirima\PimcoreKeeperBundle\Service\Logger;
 use Pipirima\PimcoreKeeperBundle\Service\StringService;
 use Pipirima\PimcoreKeeperBundle\Service\MailerService;
 
-class ClassEventHandler implements EventHandlerInterface
+class DataObjectEventHandler implements EventHandlerInterface
 {
     protected MailerService $mailer;
 
@@ -26,23 +27,24 @@ class ClassEventHandler implements EventHandlerInterface
 
     public function handle(array $config, array $arguments)
     {
+        $this->logger->log("A");
         $funcName = $arguments[0];
 
         if (false !== strpos($funcName, "Pre")) {
             return;
         }
 
+        /** @var DataObjectEvent $event */
         $event = $arguments[1];
-        if (!$event instanceof ClassDefinitionEvent) {
+        if (!$event instanceof DataObjectEvent) {
             $message = $funcName . ": " . get_class($event);
             $this->logger->log($message);
             return;
         }
 
-        $classDefinition = $event->getClassDefinition();
-        $classId = $classDefinition->getId();
-        $className = $classDefinition->getName();
-        $textMessage = "func $funcName: class: id: $classId name: $className";
+        $obj = $event->getObject();
+        $classname = get_class($obj);
+        $textMessage = "func $funcName: classname: $classname path: {$obj->getFullPath()} ID: {$obj->getId()}";
         $this->logger->log($textMessage);
 
         $toEmail = $config['email'];
@@ -51,7 +53,7 @@ class ClassEventHandler implements EventHandlerInterface
         }
 
         $hostUrl = strval(Tool::getHostUrl());
-        $subject = $hostUrl . ": " . ($config['subject'] ?? 'Class change notification');
+        $subject = $hostUrl . ": " . ($config['subject'] ?? 'DataObject change notification');
 
         $this->logger->log("Sending mail to $toEmail subject: $subject");
         $this->mailer->send($subject, $textMessage, $toEmail);
--- src/PimcoreKeeperBundle/Service/EventListener.php
+++ src/PimcoreKeeperBundle/Service/EventListener.php
@@ -15,11 +15,14 @@ class EventListener
 
     protected StringService $stringService;
 
-    public function __construct(EventHandlerFactory $factory, ConfigService $config, StringService $stringService)
+    protected Logger $logger;
+
+    public function __construct(EventHandlerFactory $factory, ConfigService $config, StringService $stringService, Logger $logger)
     {
         $this->factory = $factory;
         $this->config = $config;
         $this->stringService = $stringService;
+        $this->logger = $logger;
     }
 
     private function isFuncNameMatchingEventsName(string $funcName, array $eventsNames): bool
@@ -40,16 +43,15 @@ class EventListener
      */
     public function __call(string $funcName, array $funcArguments)
     {
+        $this->logger->log("=====================================================================");
+        $this->logger->log("event funcName: $funcName");
         array_unshift($funcArguments, $funcName);
         /** @var EventHandlerInterface $handler */
         foreach ($this->factory->getHandlers() as $handler) {
             foreach ($this->config->getConfig()['alerts'] as $alertConfig) {
-
-                if (!$this->isFuncNameMatchingEventsName($funcName, $alertConfig['events'])) {
-                    return;
+                if ($this->isFuncNameMatchingEventsName($funcName, $alertConfig['events'])) {
+                    $handler->handle($alertConfig, $funcArguments);
                 }
-
-                $handler->handle($alertConfig, $funcArguments);
             }
         }
     }
